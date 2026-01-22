fdf9a04b8db57cf426c1d13cc59ad5a37434d9e8
footility <paolo.mistretta@footility.com>
2026-01-22 16:33:39 +0100

    A05 - Fix metodo invoice() duplicato in PaymentPlan

--- app/Models/PaymentPlan.php
+++ app/Models/PaymentPlan.php
@@ -36,11 +36,6 @@ class PaymentPlan extends Model
         return $this->belongsTo(Payment::class);
     }
 
-    public function invoice()
-    {
-        return $this->belongsTo(Invoice::class);
-    }
-
     public function scopePending($query)
     {
         return $query->where('status', 'pending');
